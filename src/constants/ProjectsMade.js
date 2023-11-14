6172f3d4db590c655c96c031ebb23377605d110c
mariano <marianoarias98@gmail.com>
2023-11-14 09:19:02 -0300

    Update projects information

--- src/constants/ProjectsMade.js
+++ src/constants/ProjectsMade.js
@@ -1,30 +1,28 @@
 export const ProjectsMade = [
     {
-        title: 'Proyecto 1',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
-        image: 'https://picsum.photos/200/300',
-        tecnologies: [
-            'Javascript',
-            'Vue'
-        ],
-        url: ''
-    }, {
-        title: 'Proyecto 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
+        title: 'Conectando Viviendas, Uniendo Personas',
+        description: 'Proyecto que redefine la búsqueda y oferta de viviendas. Inquilinos encuentran su hogar ideal; arrendadores ofrecen propiedades de manera eficiente. Integrando funciones de búsqueda, publicaciones y mapas para una experiencia completa.',
         image: 'https://picsum.photos/200/400',
         tecnologies: [
-            'Javascript',
-            'Vue'
+            'html',
+            'css',
+            'laravel',
+            'vue js',
         ],
-        url: ''
-    }, {
-        title: 'Proyecto 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.',
-        image: 'https://picsum.photos/200/500',
+        url: '',
+        state: 'En desarrollo..'
+    },
+    {
+        title: 'Calendario de Eventos',
+        description: 'Calendario Interactivo con la capacidad de gestionar eventos a tu medida, desde la adición y edición hasta el seguimiento de logros y asistencias, esta solución intuitiva redefine la organización personal. Calendario diseñado para adaptarse a cada ritmo de vida, facilitando la planificación y celebración de momentos especiales.',
+        image: 'https://picsum.photos/200/300',
         tecnologies: [
-            'Javascript',
-            'Vue'
+            'html',
+            'css',
+            'vue js',
+            'local storage'
         ],
-        url: ''
-    }
+        url: '',
+        state: 'En desarrollo..'
+    }, 
 ]
