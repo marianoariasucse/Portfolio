7210972a453913f0cc5da899efe69a82054a9d1c
mariano <marianoarias98@gmail.com>
2023-11-14 09:26:19 -0300

    agregado un nuevo tipo de boton y editado la card de proyectos

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -15,14 +15,25 @@ const props = defineProps({
     default: 'primary'
   }
 })
- 
+
 const getClasses = () => {
-  if (props.variant === 'primary') {
-    return 'mt-8 text-lg sm:text-xl bg-gray-800 p-4 rounded transition-colors hover:bg-white hover:text-gray-800 font-semibold';
-  } else if (props.variant === 'secondary') {
-    return 'text-m bg-white border-blue-500 text-blue-600 p-2 hover:bg-blue-500 hover:text-white rounded font-semibold';
+  let buttonClass;
+
+  switch (props.variant) {
+    case 'primary':
+      buttonClass = 'mt-8 text-lg sm:text-xl bg-gray-800 p-4 rounded transition-colors hover:bg-white hover:text-gray-800 font-semibold';
+      break;
+    case 'secondary':
+      buttonClass = 'text-m bg-white border-blue-500 text-blue-600 p-2 hover:bg-blue-500 hover:text-white rounded font-semibold';
+      break;
+    case 'tertiary':
+      buttonClass = 'text-m bg-gray-600 text-gray-200 p-2 hover:bg-blue-500 hover:text-white rounded font-semibold';
+      break;
+    default:
+      buttonClass = 'text-m bg-white border-blue-500 text-blue-600 p-2 hover:bg-blue-500 hover:text-white rounded font-semibold';
+      break;
   }
 
-  return '';
-}
+  return buttonClass;
+};
 </script>
--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -3,18 +3,24 @@
     <h2 class="text-3xl sm:text-5xl font-bold text-center text-gray-200">Proyectos</h2>
     <div class="mt-8 grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
       <div v-for="project in ProjectsMade" key="project.title"
-        class="bg-gray-800 shadow-md rounded-md p-4 transition-transform duration-500 ease-in-out transform hover:scale-105">
+        class="bg-gray-800 shadow-md rounded-md p-4 mb-4 transition-transform duration-500 ease-in-out transform hover:scale-105">
         <img :src="project.image" width="200" height="120" alt="Proyecto 1" class="mx-auto mb-4 w-full" loading="lazy"
           style="aspect-ratio: 120/ 120; object-fit: cover;" />
         <h3 class="text-xl font-medium text-gray-200">{{ project.title }}</h3>
         <p class="mt-2 mb-4 text-sm text-gray-400">{{ project.description }}</p>
-        <div class="flex gap-2 items-center">
+        <div class="flex gap-2 mb-3 items-center">
           <p class="text-gray-100">Tecnologias usadas: </p>
           <p class="mt-2 mb-4 text-sm text-gray-200 bg-gray-600 p-1 rounded" v-for="tecnology in project.tecnologies">
             {{ tecnology }}
           </p>
         </div>
-        <Button text="Vista Previa" variant="secondary" />
+        <div class="flex justify-between items-center">
+          <div class="flex gap-2">
+            <Button text="Vista Previa" variant="secondary" />
+            <Button text="Ver Codigo" variant="tertiary" />
+          </div>
+          <p class="text-gray-200">{{ project.state }}</p>
+        </div>
       </div>
     </div>
   </section>
